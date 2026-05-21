5728ee2670d832bc1dff24aa93d5a662145da5f2
LYNNE\LENOVO <sofiarose.calarion@gmail.com>
2026-05-21 15:18:33 +0800

    - Removed unused Home glue class - Added few comments to locators

--- cucumber-java-selenium/src/test/java/io/cucumber/locators/LocatorElements.java
+++ cucumber-java-selenium/src/test/java/io/cucumber/locators/LocatorElements.java
@@ -6,6 +6,7 @@ public class LocatorElements {
 
     public static final By dynamicAText = By.xpath("//a[contains(text(), '%s')]");
     public static final By dynamicPText = By.xpath("//p[contains(text(), '%s')]");
+    // matches a table cell by row number and column header name
     public static final By dynamicTableTrTdTheadSpan = By.xpath("//table[@id='table1']//tbody/tr[%s][td[position()=(count(//table[@id='table1']//thead//th[span[normalize-space(.)='%s']]/preceding-sibling::th)+1)][normalize-space(.)='%s']]");
     public static final By dynamicDivAText = By.xpath("//div[contains(@id,'content')]//ul//li[%s]//a");
 }
