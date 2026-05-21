91d0ac6ac652d93c5dfebeb9951edc997a0b086e
LYNNE\LENOVO <sofiarose.calarion@gmail.com>
2026-05-21 15:38:05 +0800

    - Fixed logs for clearer view

--- cucumber-java-selenium/src/test/java/io/cucumber/glue/HomepageListsSteps.java
+++ cucumber-java-selenium/src/test/java/io/cucumber/glue/HomepageListsSteps.java
@@ -17,17 +17,24 @@ public class HomepageListsSteps extends Context {
 
     @Then("the homepage displays the expected list of examples")
     public void homepageLists () throws Exception {
+    	
         PropertyFile.getPropertyFile("homepagelists", "homepage-lists");
         ArrayList<String> arrActual = new ArrayList<>();
         String rawExpected = PropertyFile.getAttribute("expected.links");
         String[] strExpected = rawExpected.split(",");
 
         // loop through each link on the homepage by index
-        for (int i = 1; i <= strExpected.length; i++) {
+        for (int i = 1; i <= 44; i++) {
             String strValue = Base.getValue(String.valueOf(i), CommonTypes.DIV_ATEXT);
             arrActual.add(strValue);
         }
-
+        for (int i = 0; i < strExpected.length; i++) {
+            if (!strExpected[i].equals(arrActual.get(i))) {
+            	Base.enterLog("Mismatch at index " + i);
+            	Base.enterLog("Expected: " + strExpected[i]);
+            	Base.enterLog("Actual  : " + arrActual.get(i));
+            }
+        }
         Base.assertThat(arrActual, strExpected);
     }
 }
